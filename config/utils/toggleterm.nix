50450b9590ba2c4328fa71c7cefdd40ef66b6367
hozhai <zhaihongmeng@gmail.com>
2024-09-28 17:01:23 -0300

    Fish!

--- config/utils/toggleterm.nix
+++ config/utils/toggleterm.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       size = ''
-        function(term)
+       function(term)
           if term.direction == "horizontal" then
             return 15
               elseif term.direction == "vertical" then
@@ -19,7 +19,7 @@
       persist_mode = true;
       insert_mappings = true;
       close_on_exit = true;
-      shell = "zsh";
+      shell = "fish";
       direction = "horizontal"; # 'vertical' | 'horizontal' | 'window' | 'float'
       auto_scroll = true;
       float_opts = {
@@ -39,19 +39,19 @@
     };
   };
 
-  # extraConfigLua = ''
-  #   function _G.set_terminal_keymaps()
-  #   	local opts = { buffer = 0 }
-  #   	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  #   	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-  #   	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  #   	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  #   	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  #   	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-  #   	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-  #   end
-  #
-  #   -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-  #   -- vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
-  # '';
+  extraConfigLua = ''
+    function _G.set_terminal_keymaps()
+    	local opts = { buffer = 0 }
+    	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+    	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+    	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+    	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+    	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+    	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+    	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    -- vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+  '';
 }
