8c7e242077675882b12ce258309cd69c52e40c2c
hozhai <zhaihongmeng@gmail.com>
2024-09-29 20:37:40 -0300

    pain

--- config/lsp/conform.nix
+++ config/lsp/conform.nix
@@ -5,40 +5,22 @@
       notifyOnError = true;
       formattersByFt = {
         html = [
-          [
             "prettierd"
-            "prettier"
-          ]
         ];
         css = [
-          [
             "prettierd"
-            "prettier"
-          ]
         ];
         javascript = [
-          [
             "prettierd"
-            "prettier"
-          ]
         ];
         javascriptreact = [
-          [
             "prettierd"
-            "prettier"
-          ]
         ];
         typescript = [
-          [
             "prettierd"
-            "prettier"
-          ]
         ];
         typescriptreact = [
-          [
             "prettierd"
-            "prettier"
-          ]
         ];
         java = [ "google-java-format" ];
         python = [ "black" ];
@@ -52,25 +34,6 @@
         ];
         rust = [ "rustfmt" ];
       };
-      format_on_save = ''
-        function(bufnr)
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-
-          if slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-
-          local function on_format(err)
-            if err and err:match("timeout$") then
-              slow_format_filetypes[vim.bo[bufnr].filetype] = true
-            end
-          end
-
-          return { timeout_ms = 200, lsp_fallback = true }, on_format
-         end
-      '';
     };
   };
 
@@ -106,35 +69,40 @@
   ];
   extraConfigLua = ''
     local conform = require("conform")
-    local notify = require("notify")
 
-    conform.setup()
+    conform.setup({
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
 
-    local function show_notification(message, level)
-      notify(message, level, { title = "conform.nvim" })
-    end
-
-    vim.api.nvim_create_user_command("FormatToggle", function(args)
-      local is_global = not args.bang
-      if is_global then
-        vim.g.disable_autoformat = not vim.g.disable_autoformat
-        if vim.g.disable_autoformat then
-          show_notification("Autoformat-on-save disabled globally", "info")
-        else
-          show_notification("Autoformat-on-save enabled globally", "info")
-        end
-      else
-        vim.b.disable_autoformat = not vim.b.disable_autoformat
-        if vim.b.disable_autoformat then
-          show_notification("Autoformat-on-save disabled for this buffer", "info")
-        else
-          show_notification("Autoformat-on-save enabled for this buffer", "info")
-        end
-      end
-    end, {
-        desc = "Toggle autoformat-on-save",
-        bang = true,
-      })
+    -- local notify = require("notify")
+    -- local function show_notification(message, level)
+    --   notify(message, level, { title = "conform.nvim" })
+    -- end
+    --
+    -- vim.api.nvim_create_user_command("FormatToggle", function(args)
+    --   local is_global = not args.bang
+    --   if is_global then
+    --     vim.g.disable_autoformat = not vim.g.disable_autoformat
+    --     if vim.g.disable_autoformat then
+    --       show_notification("Autoformat-on-save disabled globally", "info")
+    --     else
+    --       show_notification("Autoformat-on-save enabled globally", "info")
+    --     end
+    --   else
+    --     vim.b.disable_autoformat = not vim.b.disable_autoformat
+    --     if vim.b.disable_autoformat then
+    --       show_notification("Autoformat-on-save disabled for this buffer", "info")
+    --     else
+    --       show_notification("Autoformat-on-save enabled for this buffer", "info")
+    --     end
+    --   end
+    -- end, {
+    --     desc = "Toggle autoformat-on-save",
+    --     bang = true,
+    --   })
     '';
 }
 
