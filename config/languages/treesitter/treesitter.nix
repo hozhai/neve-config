b185e1f54df34f2d37bcac23f38f0dd75dcaa48e
hozhai <zhaihongmeng@gmail.com>
2024-09-29 19:41:12 -0300

    added treesitter langs

--- config/languages/treesitter/treesitter.nix
+++ config/languages/treesitter/treesitter.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   plugins.treesitter = {
     enable = true;
     settings = {
@@ -6,5 +6,27 @@
     };
     folding = true;
     nixvimInjections = true;
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      bash
+      c
+      cpp
+      css
+      dockerfile
+      go
+      graphql
+      html
+      javascript
+      json
+      lua
+      nix
+      python
+      regex
+      ruby
+      rust
+      svelte
+      toml
+      typescript
+      yaml
+    ];
   };
 }
